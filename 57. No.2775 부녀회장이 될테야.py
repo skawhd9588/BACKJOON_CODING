a4a763c40cbcd9c6479cd2478231201ef7237a58
skawhd9588 <54003598+skawhd9588@users.noreply.github.com>
2022-05-04 23:43:25 +0900

    Update 57. No.2775 부녀회장이 될테야.py

--- 57. No.2775 부녀회장이 될테야.py
+++ 57. No.2775 부녀회장이 될테야.py
@@ -1,12 +1,12 @@
-arr = []
 for i in range(int(input())) :
+    arr = []
     k = int(input())
     n = int(input())
-    for j in range(k) :
+    for j in range(k+1) :
         arr.append([1])
-    for j in range(n-1) :
-        arr[0].append(j+2)
-    for a in range(1,k) :
+    for j in range(1, n) :
+        arr[0].append(j+1)
+    for a in range(1,k+1) :
         for b in range(1,n) :
             arr[a].append(arr[a][b-1] + arr[a-1][b])
-    print(arr[k][n])
+    print(arr[k][n-1])
